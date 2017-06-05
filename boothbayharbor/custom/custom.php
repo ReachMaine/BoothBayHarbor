302b09dbaef8c098eb30395e2d0e166aa39a621d
Linda "zig" Speight <linda@reachmaine.com>
2017-06-05 07:37:31 -0400

    hide streetview & inside view  on location metabox.

--- boothbayharbor/custom/custom.php
+++ boothbayharbor/custom/custom.php
@@ -174,3 +174,12 @@ function bbh_inventor_submission($steps, $post_type) {
  		'petfriendly' => $steps['petfriendly'],
  );
 }
+// disable street view
+add_filter( 'inventor_metabox_field_enabled', 'disable_gmap_views', 10, 4 );
+function disable_gmap_views( $enabled, $metabox_id, $field_id, $post_type ) {
+    if ( ( 'listing_street_view' == $field_id ) || ('listing_inside_view' == $field_id) ) {
+        return false;
+    }
+
+    return $enabled;
+}
